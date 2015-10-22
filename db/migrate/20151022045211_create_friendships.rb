d5df00f4bacee8bb9f183fa8c2d8b217961a514e
darkherald <mrshi1992@gmail.com>
2015-10-22 10:43:23 -0700

    change friendship table

--- db/migrate/20151022045211_create_friendships.rb
+++ db/migrate/20151022045211_create_friendships.rb
@@ -1,9 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-        t.integer : uid1
-        t.integer : uid2
-        t.integer : status  # 0 not friend, 1 is friend, 2 pending
+        t.integer :uid1
+        t.integer :uid2
+        t.integer :status  # 0 not friend, 1 is friend, 2 pending
         t.timestamps null: false
 
         t.references :user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016220029) do
+ActiveRecord::Schema.define(version: 20151022045211) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "uid1"
+    t.integer  "uid2"
+    t.integer  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
