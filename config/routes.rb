1b4d30072a69b29e1a5e4e88dc41acb9c4ebb7f2
Haosheng Zou <zouhaosheng@gmail.com>
2015-10-23 10:18:00 -0700

    add bootstrap gem to Gemfile; add 3 pics as jumbotron background image to assets/images add css rule for signup/login page in application.css.scss add css rule for welcome page in application.css.scss
    pending: redirect to googlmap page when click Learn more on jumbotron.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'googlmap' => 'welcome#googlmap'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
