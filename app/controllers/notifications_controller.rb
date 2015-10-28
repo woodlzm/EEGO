36c30187c930e04a659738aa272b604bfa1fd92e
darkherald <mrshi1992@gmail.com>
2015-10-27 17:30:14 -0700

    Minor

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,6 +7,7 @@ class NotificationsController < ApplicationController
   def create
     @notification = Notification.create(:sender_user_id => notification_params[:sender_user_id], :message => User.find(notification_params[:userid]).email + " want to add you as a friend", :user_id => notification_params[:receiver_user_id] )
     @notification.save
+
   end
 
   private
