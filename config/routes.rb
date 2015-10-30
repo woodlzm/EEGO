74a198ed1c2d0f108176c5cd69794763db76939f
darkherald <mrshi1992@gmail.com>
2015-10-30 15:48:52 -0700

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get '/notification' => 'notifications#new', as: :notification
+  get 'notification' => 'notifications#index', as: :notification
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
