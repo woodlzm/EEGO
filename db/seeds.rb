7f463ac03c2f9fac0706e4e95baf28aeaf8fb9f9
darkherald <mrshi1992@gmail.com>
2015-11-20 14:24:10 -0800

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#t1= User.create(email: "mrshi1994@gmail.com", encrypted_password: "$2a$10$iz/HOog0t0EB4Fbt.f6qNe4l0JC4kzS5v.Nn2fJDUMCCM0/sgFMwO")
-#Notification.create(userid: 1, message: "have a try", user_id: t1.id)
+puts "start seeding"
+
+number_of_advenetures = 100
+number_of_users = 100
+
+def
+create_adventure(location_creater)
+  Adventure.create(
+      location: location_creater,
+      num_people: rand(1..99),
+      num_day: rand(1..99),
+      plan: "Hiking" + rand(1..5).to_s + "places",
+      user_id: rand(1..99)
+  )
+end
 
 
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "a", num_people: 3, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 4, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "b", num_people: 5, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "b", num_people: 34, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "c", num_people: 1, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "c", num_people: 10, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "a", num_people: 4, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "b", num_people: 6, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "c", num_people: 7, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 8, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "b", num_people: 70, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "c", num_people: 23, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "a", num_people: 12, num_day: 3, plan: "Hiking", user_id: 1);
+
+# create adventures
+puts "Create adventures with location a"
+number_of_advenetures.times do
+  create_adventure("a")
+end
+
+puts "Create adventures with location b"
+number_of_advenetures.times do
+  create_adventure("b")
+end
+
+puts "Create adventures with location c"
+number_of_advenetures.times do
+  create_adventure("c")
+end
+
+# create users
+puts "Create users"
+number_of_users.times do |n_user|
+  User.create(
+      email: n_user.to_s + "@ucla.edu",
+      password: "11111111",
+      password_confirmation: "11111111",
+  )
+end
+
+puts "end seeding"
