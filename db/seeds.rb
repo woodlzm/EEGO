052615bba38edd05d6f1e7a5142fefa95dc507e2
Haosheng Zou <zouhaosheng@gmail.com>
2015-12-01 11:05:39 -0800

    i

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#t1= User.create(email: "mrshi1994@gmail.com", encrypted_password: "$2a$10$iz/HOog0t0EB4Fbt.f6qNe4l0JC4kzS5v.Nn2fJDUMCCM0/sgFMwO")
-#Notification.create(userid: 1, message: "have a try", user_id: t1.id)
-
-
-=======
->>>>>>> 8bbf7c62d379f4185440b07d003df6a3a24e768c
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "a", num_people: 3, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 4, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "b", num_people: 5, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "b", num_people: 34, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "c", num_people: 1, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "c", num_people: 10, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 2, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "a", num_people: 4, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "b", num_people: 6, num_day: 3, plan: "Hiking", user_id: 4);
-Adventure.create(location: "c", num_people: 7, num_day: 3, plan: "Hiking", user_id: 3);
-Adventure.create(location: "a", num_people: 8, num_day: 3, plan: "Hiking", user_id: 1);
-Adventure.create(location: "b", num_people: 70, num_day: 3, plan: "Hiking", user_id: 2);
-Adventure.create(location: "c", num_people: 23, num_day: 3, plan: "Hiking", user_id: 1);
-<<<<<<< HEAD
-Adventure.create(location: "a", num_people: 12, num_day: 3, plan: "Hiking", user_id: 1);
-=======
-Adventure.create(location: "a", num_people: 12, num_day: 3, plan: "Hiking", user_id: 1);
->>>>>>> 8bbf7c62d379f4185440b07d003df6a3a24e768c
-=======
 puts "start seeding"
 
 number_of_advenetures = 100
@@ -78,4 +49,3 @@ number_of_users.times do |n_user|
 end
 
 puts "end seeding"
->>>>>>> 7d05f9cb7d074d7b841f50c0e95e36db2c6be474
